ca5d684d40eca02cd0daa1a799e42a8142e49aba
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-06-09 23:42:53 -0300

    layout: ajusta opção de login do menu

--- public/components/navbar.php
+++ public/components/navbar.php
@@ -8,21 +8,18 @@
                 <a href="./#stock">
                     <li class="main-menu__item">Nossos Produtos</li>
                 </a>
-
-                <a href="./login">
-                    <li class="main-menu__item">Atualizar Veículos</li>
-                </a>
                 <?php
                 if ($_SESSION) {
                 ?>
-                    <div class="main-menu__user">
+                    <a href="internal">
                         <li class="main-menu__item"><?php echo explode(" ", $_SESSION['userInfo']['name'])[0]; ?></li>
-                        <ul class="main-menu__user-options">
-                            <li href="/internal">Perfil</li>
-                            <li href="/internal">Trocar senha</li>
-                            <li onclick="logout()">Logout</li>
-                        </ul>
-                    </div>
+                    </a>
+                <?php
+                } else {
+                ?>
+                    <a href="./login">
+                        <li class="main-menu__item">Login</li>
+                    </a>
                 <?php
                 }
                 ?>
