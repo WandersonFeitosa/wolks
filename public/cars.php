d2dc81f86d501b6eece705293685ec23ddb3ca8d
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-06-17 12:21:04 -0300

    layout: centraliza pagina de carro

--- public/cars.php
+++ public/cars.php
@@ -15,41 +15,41 @@ if ($car_id) {
 }
 
 ?>
-<?php
-if ($car_id) { ?>
-    <div class="container">
-        <section class="buy">
-            <div class="buy__image">
-                <img src="<?php echo $car_info['image_url'] ?>" alt="">
+<section class='buy menu-offset' <?php echo (!$car_id) ? "style='display:block;'" : ''; ?>>
+
+    <?php
+    if ($car_id) { ?>
+        <div class="container">
+            <div class="buy__wrapper">
+                <div class="buy__image">
+                    <img src="<?php echo $car_info['image_url'] ?>" alt="">
+                </div>
+                <div class="buy__info">
+
+                    <div class="buy__info__title">
+                        <span><?php echo $car_info['model'] ?></span>
+                    </div>
+                    <div class="buy__info__stock">
+                        <span>
+                            <?php echo $car_info['stock'] ?> unidades em estoque
+                        </span>
+                    </div>
+                    <div class="buy__info__year">
+                        <span><?php echo $car_info['year'] ?></span>
+                    </div>
+                    <div class="buy__info__details">
+                        <span><?php echo $car_info['info'] ?></span>
+                    </div>
+                    <div class="buy__info__price">
+                        <span>R$ <?php echo $car_info['price'] ?></span>
+                    </div>
+                    <div class="buy__info__button">
+                        <button>Comprar</button>
+                    </div>
+                </div>
             </div>
-            <div class="buy__info">
-
-                <div class="buy__info__title">
-                    <span><?php echo $car_info['model'] ?></span>
-                </div>
-                <div class="buy__info__stock">
-                    <span>
-                        <?php echo $car_info['stock'] ?> unidades em estoque
-                    </span>
-                </div>
-                <div class="buy__info__year">
-                    <span><?php echo $car_info['year'] ?></span>
-                </div>
-                <div class="buy__info__details">
-                    <span><?php echo $car_info['info'] ?></span>
-                </div>
-                <div class="buy__info__price">
-                    <span>R$ <?php echo $car_info['price'] ?></span>
-                </div>
-                <div class="buy__info__button">
-                    <button>Comprar</button>
-                </div>
-
-        </section>
-    </div>
-<?php } else {
-    echo "<section class='menu-offset'>";
-    include "./components/cars-list.php";
-    echo "</section>";
-} ?>
+        <?php } else {
+        include "./components/cars-list.php";
+    } ?>
+</section>
 <?php include "./components/footer.php" ?>
